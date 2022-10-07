af1291a70ddd2921d766eba13a23166d039a888e
Eric Evans <eric@domainlanguage.com>
2022-10-06 20:41:32 -0400

    Character can 'grab' objects with 'E' key
    Partial solution:
    Can pick up, but can't put down, objects with 'ObjectGrabable' script.
    Look at the object, be within reachDistance, press 'E' key.
    Combine with previous commit

---
+++ Assets/_InputSystem/ObjectInteraction.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+
+public class ObjectInteraction : MonoBehaviour
+{
+    [SerializeField] private Transform playerCameraTransform;
+    [SerializeField] private LayerMask grabbableLayerMask;
+    [SerializeField] private Transform grabbedObjectHoldPoint;
+    [SerializeField] private float reachDistance = 4f; // How far can the character reach to interact with something.
+    // Camera follow distance (default 4f) should be added to reachDistance to see if it is in reach.
+    private float camFollowDistance = 4f; // TODO Should be obtained from the camera, probably.
+
+    private PlayerInput playerInput;
+    void Start()
+    {
+        Debug.Log("ObjectInteraction.Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInput == null)
+        {
+            playerInput = GetComponent<PlayerInput>();
+            Debug.Log($"playerInput = {playerInput}");
+            Debug.Log($"defaultActionMap = {playerInput.defaultActionMap}");
+            Debug.Log($"currentActionMap = {playerInput.currentActionMap}");
+            Debug.Log($"actions = {playerInput.actions}");
+            InputAction grabRelease = playerInput.actions["GrabRelease"];
+            Debug.Log($"grabRelease = {grabRelease}");
+
+ //           grabRelease.started += context => Debug.Log($"{context.action} started");
+            grabRelease.performed += context => Debug.Log($"{context.action} performed");
+            //           grabRelease.canceled += context => Debug.Log($"{context.action} canceled");
+
+            InputAction jump = playerInput.actions["Jump"];
+            Debug.Log($"jump = {jump}");
+            jump.performed += context => Debug.Log($"{context.action} performed");
+
+        }
+    }
+
+    public void OnGrabRelease()
+    {
+        Debug.Log("ObjectInteraction.OnGrabRelease");
+        ObjectGrabbable grabbable = FindGrabbableInReach();
+        Debug.Log($"grabbable = {grabbable}");
+        if (grabbable != null) grabbable.Grab(grabbedObjectHoldPoint);
+    }
+
+    public ObjectGrabbable FindGrabbableInReach()
+    {
+        float raycastDistance = camFollowDistance + reachDistance;
+        bool hadHit = Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out RaycastHit raycastHit, raycastDistance, grabbableLayerMask);
+        Debug.Log($"hadHit = {hadHit}");
+        Debug.Log($"raycastHit = {raycastHit}");
+        if (!hadHit) return null;
+        if (!raycastHit.transform.TryGetComponent<ObjectGrabbable>(out ObjectGrabbable grabbable))
+            return null;
+        return grabbable;
+    }
+
+}
